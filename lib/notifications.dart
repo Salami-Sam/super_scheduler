ad3b16518bb3ae84db148dd0a5902f392a84f11d
Rudy Fisher <rfisherd7@zoho.com>
2021-03-12 16:05:34 -0600

    implemented notifications page with simple listview and listitems.

--- lib/notifications.dart
+++ lib/notifications.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
-import 'drawer.dart';
 
-/* Screens:
- * Notifications
- */
+class NotificationsWidget extends StatefulWidget {
+  @override
+  _NotificationsWidgetState createState() => _NotificationsWidgetState();
+}
 
-class NotificationsWidget extends StatelessWidget {
+class _NotificationsWidgetState extends State<NotificationsWidget> {
+  final userNotifications = [
+    'tmp',
+    'tmp',
+    'tmp',
+    'tmp',
+    'tmp',
+    'tmp',
+    'tmp',
+    'tmp',
+  ];
   @override
   Widget build(BuildContext context) {
-    return Text('temp');
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: ListView.separated(
+        itemCount: userNotifications.length,
+        itemBuilder: (BuildContext context, int index) => ListTile(
+          leading: Icon(Icons.notification_important_rounded),
+          title: Text(
+            userNotifications[index],
+          ),
+          subtitle: Text(
+            userNotifications[index],
+          ),
+          trailing: IconButton(
+              icon: Icon(Icons.delete_forever_rounded), onPressed: null),
+        ),
+        separatorBuilder: (context, index) => Divider(
+          height: 16.0,
+        ),
+      ),
+    );
   }
 }
+
+//TODO: define a class for notifications with optional buttons
